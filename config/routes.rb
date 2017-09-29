5feb9e2c84209891f8fe6ac7585cd6b4e8d0dab8
koji-1582 <katokoji@katoukoujinoMacBook.local>
2017-09-30 00:04:31 +0900

    sample commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: {
+  registrations: "users/registrations",
+  omniauth_callbacks: "users/omniauth_callbacks"
+  }
   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
   if Rails.env.development?
    mount LetterOpenerWeb::Engine, at: "/letter_opener"
