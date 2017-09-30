b4d14aa262d151c288ac3aeeb3c188367979fbd9
koji-1582 <katokoji@katoukoujinoMacBook.local>
2017-09-30 20:42:47 +0900

    remend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
    @post = Post.new(posts_params)
    @post.user_id = current_user.id
    if @post.save
-     redirect_to posts_path, notice: "complete！"
+     redirect_to posts_path, notice: "投稿しました！"
      NoticeMailer.sendmail_post(@post).deliver
    else
      render 'new'
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
 
  def update
    if @post.update(posts_params)
-     redirect_to posts_path, notice: "complete！"
+     redirect_to posts_path, notice: "更新しました！"
    else
      render 'edit'
    end
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
 
  def destroy
    @post.destroy
-   redirect_to posts_path, notice: "complete！"
+   redirect_to posts_path, notice: "削除しました！"
  end
 
 
